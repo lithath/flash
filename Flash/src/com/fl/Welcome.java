923340653eecb16f0d7345195f03cecbadaa2062
littishathankam <littishathankam@HP>
2018-01-19 12:36:39 +0530

    initial

---
+++ Flash/src/com/fl/Welcome.java
@@ -0,0 +1,12 @@
+package com.fl;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("jhdfjfhj");
+
+System.out.println("welcome 1234");
+	}
+
+}
